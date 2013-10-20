ef46e8b8a41c72af3be5166774e14113b859797f
javierGaliana <javier.galiana.romero@gmail.com>
2013-10-21 00:15:43 +0200

    practica 1 a practica1

--- practica1/configuracion.php
+++ practica1/configuracion.php
@@ -1,6 +1,6 @@
 <?php
-  define("DB_DSN", "mysql:host=localhost;dbname=x48569198" ); 
-  define("DB_USUARIO", "x48569198" ); 
-  define("DB_CONTRASENIA", "x48569198" ); 
+  define("DB_DSN", "mysql:host=tunnel.pagodabox.com:3306;dbname=periodico" ); 
+  define("DB_USUARIO", "dierdre" ); 
+  define("DB_CONTRASENIA", "fWjetzc3" ); 
 ?>
 
