532c3e5bb4646cc525cc0d7cad7236b11f2e8756
javierGaliana <javier.galiana.romero@gmail.com>
2013-10-21 01:26:11 +0200

    configuracion.php

--- practica1/configuracion.php
+++ practica1/configuracion.php
@@ -1,5 +1,5 @@
 <?php
-  define("DB_DSN", "mysql:host=tunnel.pagodabox.com:3306;dbname=periodico" ); 
+  define("DB_DSN", "mysql:host=tunnel.pagodabox.com;dbname=periodico;port=3306" ); 
   define("DB_USUARIO", "dierdre" ); 
   define("DB_CONTRASENIA", "fWjetzc3" ); 
 ?>
