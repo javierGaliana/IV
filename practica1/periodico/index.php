160d958d34a1eeab89481fe08b60af9886612071
javierGaliana <javier.galiana.romero@gmail.com>
2013-10-21 01:56:38 +0200

    configuracion.php

--- practica1/periodico/index.php
+++ practica1/periodico/index.php
@@ -7,7 +7,7 @@
     <title> Diario INDEP, información veraz. </title>
     <script type="text/javascript" src="../javascript/funciones.js"> </script>
     <?php
-       echo $_SERVER['db1_HOST'];
+       echo $_SERVER['DB1_HOST'];
        include_once('../configuracion.php');
 
         
