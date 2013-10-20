210ecd84b8ce3ab3656639cef57c81467a91fa60
javierGaliana <javier.galiana.romero@gmail.com>
2013-10-21 00:07:00 +0200

    practica 1 a practica1

---
+++ practica1/ultima.sql
@@ -0,0 +1,5 @@
+insert into nuevasNoticias set usuario='admin', texto='Acercamiento entre Rusia y Japón. El presidente ruso, Vladimir Putin, y el primer ministro japonés, Shinzo Abe, han expresado su intención de acabar con una disputa por unas islas del Pacífico que ha impedido la firma de un tratado de paz desde hace 67 años. Informa AFP. ';
+
+insert into nuevasNoticias set usuario='admin', texto='Afganistán recibió fondos de la CIA. Así lo ha reconocido el presidente afgano, Hamid Karzai. "Afganistán ha recibido dinero de la CIA durante los últimos 10 años, aunque no ha sido una gran cantidad", ha confirmado en un comunicado, según AFP. ';
+
+insert into nuevasNoticias set usuario='admin', texto='El recién nombrado primer ministro italiano, Enrico Letta, ha manifestado la urgencia de implantar medidas de crecimiento para revertir la política de austeridad de su antecesor. "Italia está muriendo de austeridad. Las políticas de crecimiento no pueden esperar". Informa AFP. ';
