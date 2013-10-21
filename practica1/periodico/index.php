48968ef8c0eb08b9c7067796599bda0f12c96adf
javierGaliana <javier.galiana.romero@gmail.com>
2013-10-21 04:29:16 +0200

    Boxfile

--- practica1/periodico/index.php
+++ practica1/periodico/index.php
@@ -10,9 +10,9 @@
        include_once('../configuracion.php');
 
         
-       session_start();
+#       session_start();
        //añadir una noticia a últimas novedades
-       if($_SESSION["usuario"] == "admin"){
+/*       if($_SESSION["usuario"] == "admin"){
           if(!empty($_POST["nombreAniadir"])){
              try{$conexion = new PDO(DB_DSN,DB_USUARIO,DB_CONTRASENIA);}catch(PDOException $e){ echo"No se pudo realizar la conexión: ".$e->getMessage();}
                $consulta = "Insert into nuevasNoticias set usuario='".$_POST["nombreAniadir"]."',texto='".$_POST["nuevaNoticia"]."';";
@@ -27,11 +27,11 @@
           if(!empty($_POST["pag"])){
               echo "Pagina pulsada: ".$_POST["pag"];
           }
-          
+  */        
          
-       }
+ #      }
        //Barra de identificación como usuario-registro o 'log off'
-       if(!$_SESSION["usuario"]){
+/*       if(!$_SESSION["usuario"]){
          echo " 
          <script type='text/javascript'>
 	    window.onload= function(){
@@ -60,7 +60,7 @@
          </script> ";
        }
        else{
-         echo "
+ */        echo "
           <script type='text/javascript'>
 	    window.onload= function(){
             //Texto
@@ -78,7 +78,7 @@
             }
          
          </script>";
-      }
+  #    }
 
       //mostrar las noticias de novedades.
       try{$conexion = new PDO(DB_DSN,DB_USUARIO,DB_CONTRASENIA);}catch(PDOException $e){ echo"No se pudo realizar la conexión: ".$e->getMessage();}
